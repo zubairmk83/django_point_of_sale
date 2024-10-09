623252061b0fda68c61ad1813be215ea28e26f3f
Zubair Khan <zubair.mk83@gmail.com>
2024-10-09 10:36:34 +0200

    updated

--- django_pos/django_pos/settings.py
+++ django_pos/django_pos/settings.py
@@ -90,12 +90,25 @@ WSGI_APPLICATION = "django_pos.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+#DATABASES = {
+#    "default": {
+#        "ENGINE": "django.db.backends.sqlite3",
+#        "NAME": BASE_DIR / "db.sqlite3",
+#    }
+#}
+
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',  # Change to PostgreSQL backend
+        'NAME': 'postgres',               # Replace with your database name
+        'USER': 'postgres',               # Replace with your database user
+        'PASSWORD': 'postgres',       # Replace with your database password
+        'HOST': 'localhost',                         # Set to 'localhost' or '127.0.0.1' for local development
+        'PORT': '5432',                              # Default PostgreSQL port
     }
 }
+
+
 
 
 # Password validation
